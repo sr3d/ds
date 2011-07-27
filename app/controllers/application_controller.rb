10ace547ca007b6821458a936f94dbd5c16342b2
Alex Le <nworld3d@yahoo.com>
2011-07-26 22:09:35 -0700

    - update styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,9 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     if resource.is_a? User
       if resource.has_invited_friends
-        '/friends/invite'
-      else
         '/events'
+      else
+        '/friends/invite'
       end
     else
       '/'
